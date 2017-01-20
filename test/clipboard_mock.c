994a615a343e4f9450ddd3806972eec8b25eaf2b
Douman <douman@gmx.se>
2017-01-20 22:06:54 +0300

    Add mock tests for Clipboard_set

--- test/clipboard_mock.c
+++ test/clipboard_mock.c
@@ -7,6 +7,8 @@
 
 mmk_mock_define(GetClipboardData_mock, HANDLE WINAPI, UINT);
 mmk_mock_define(GlobalLock_mock, LPVOID, HGLOBAL);
+mmk_mock_define(GlobalAlloc_mock, HGLOBAL, UINT, SIZE_T);
+mmk_mock_define(SetClipboardData_mock, HANDLE, UINT, HANDLE);
 
 #define CREATE_MOCK(name) cr_assert( (mmk_fn) mmk_mock(#name "@self", name ## _mock) != MMK_MOCK_INVALID, #name " mock creation failed")
 #define MOCK_RETURN_PTR(_data, _type) &(_type){ &_data }
@@ -27,7 +29,7 @@ static void teardown() {
 TestSuite(clipboard_mock, .init = setup, .fini = teardown);
 
 /**
- * Test setting of clipboard with text.
+ * Test try to get clipboard text
  *
  * GetClipboardData mock returns NULL.
  */
@@ -46,7 +48,7 @@ Test(clipboard_mock, get_fail_no_data) {
 }
 
 /**
- * Test setting of clipboard with text.
+ * Test try to get clipboard text
  *
  * GetClipboardData mock returns dummy.
  * GlobalLock mock returns NULL
@@ -71,4 +73,41 @@ Test(clipboard_mock, get_fail_no_lock) {
 
     mmk_reset(GetClipboardData);
     mmk_reset(GlobalLock);
+}
+
+/**
+ * Test try to set text.
+ *
+ * GlobalAlloc mock returns NULL
+ */
+Test(clipboard_mock, set_fail_no_memory) {
+    const char text[] = "Mock!";
+    const size_t len = strlen(text) + 1;
+
+    CREATE_MOCK(GlobalAlloc);
+    mmk_when(GlobalAlloc(mmk_any(UINT), mmk_eq(SIZE_T, len)), .then_return = NULL);
+
+    cr_assert_eq(Clipboard_set_string(text), 0, "Should fail to set clipboard!");
+    cr_assert(mmk_verify(GlobalAlloc(mmk_any(UINT), mmk_eq(UINT, len)), .times = 1),
+              "GlobalAlloc incorrect invokation");
+
+    mmk_reset(GlobalAlloc);
+}
+
+/**
+ * Test try to set text.
+ *
+ * SetClipboardData mock returns NULL
+ */
+Test(clipboard_mock, set_just_fail) {
+    const char text[] = "Mock!";
+
+    CREATE_MOCK(SetClipboardData);
+    mmk_when(SetClipboardData(mmk_any(UINT), mmk_any(HANDLE)), .then_return = NULL);
+
+    cr_assert_eq(Clipboard_set_string(text), 0, "Should fail to set clipboard!");
+    cr_assert(mmk_verify(SetClipboardData(mmk_any(UINT), mmk_any(HANDLE)), .times = 1),
+              "SetClipboardData incorrect invokation");
+
+    mmk_reset(SetClipboardData);
 }
